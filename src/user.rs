b5265691e927474db1a4a57bcc6bd6dcd23b27c2
amarrerod <alemarrerodiaz@gmail.com>
2023-05-17 13:57:55 +0100

    🦺 Authentication Done

--- src/user.rs
+++ src/user.rs
@@ -1,30 +1,66 @@
-use serde::Deserialize;
+use log::info;
+use rspotify::{prelude::*, scopes, AuthCodeSpotify, Credentials, OAuth, Token};
+use std::path::Path;
 
-#[derive(Deserialize, Debug, Default)]
-pub struct AccessToken {
-    pub access_token: String,
-    pub token_type: String,
-    pub expires_in: i32,
-}
-#[derive(Debug, Default)]
-pub struct User {
-    pub id: String,
-    pub name: String,
-    pub access_token: AccessToken,
+fn load_token(path: &Path) -> Result<Token, rspotify::model::error::ModelError> {
+    info!("Loading Token from cache");
+    let token: Token = Token::from_cache(&path)?;
+    info!("Token loaded");
+    Ok(token)
 }
 
-impl User {
-    pub fn new(id: &str, name: &str, access_token: AccessToken) -> User {
-        User {
-            id: id.to_string(),
-            name: name.to_string(),
-            access_token,
-        }
-    }
+async fn log_user(
+    cache_file: &Path,
+) -> Result<AuthCodeSpotify, rspotify::model::error::ModelError> {
+    info!("Login user");
+
+    let credentials = Credentials::from_env().unwrap();
+    info!("Credentials obtained!");
+    // Using every possible scope
+    let scopes = scopes!(
+        "user-read-email",
+        "user-read-private",
+        "user-top-read",
+        "user-read-recently-played",
+        "user-follow-read",
+        "user-library-read",
+        "user-read-currently-playing",
+        "user-read-playback-state",
+        "user-read-playback-position",
+        "playlist-read-collaborative",
+        "playlist-read-private"
+    );
+    info!("Scopes selected are: {:?}", scopes);
+
+    let oauth = OAuth::from_env(scopes).unwrap();
+    let spotify = AuthCodeSpotify::new(credentials.clone(), oauth.clone());
+    let url = spotify.get_authorize_url(false).unwrap();
+    // This function requires the `cli` feature enabled.
+    spotify
+        .prompt_for_token(&url)
+        .await
+        .expect("couldn't authenticate successfully");
+
+    // Antes de terminar guardamos el nuevo token
+    info!("Saving new Access Token into cache file");
+
+    let token = spotify.get_token().lock().await.unwrap().clone().unwrap();
+    token
+        .write_cache(&cache_file)
+        .expect("Couldn't write token into cache file:");
+    info!("New Access Token saved into cache file");
+
+    Ok(spotify)
 }
 
-impl AccessToken {
-    pub fn is_valid(&self) -> bool {
-        todo!()
-    }
+pub async fn authorise_user() -> Result<AuthCodeSpotify, rspotify::model::error::ModelError> {
+    let cache_file: &Path = Path::new(rspotify::DEFAULT_CACHE_PATH);
+    let token = load_token(&cache_file).unwrap_or_default();
+
+    let spotify: AuthCodeSpotify = match token.is_expired() {
+        false => AuthCodeSpotify::from_token(token),
+        true => log_user(&cache_file).await.unwrap(),
+    };
+    info!("AuthCodeSpotify created");
+    Ok(spotify)
 }
